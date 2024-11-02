9e20f45b4875daa0d640874d4b37a6f6c66d1462
Ashan Jayasinghe <ashanjr@outlook.com>
2024-11-02 23:41:07 +0530

    db connection file : db.js established

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+//load env variables
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+//export the pool as a promise
+export default pool.promise();
