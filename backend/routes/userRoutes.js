88c0708834930b6cb4c2ba73973f1252eb39d89d
Ashan Jayasinghe <ashanjr@outlook.com>
2024-11-03 03:35:51 +0530

    user routes for handling thrid party users finished

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,38 @@
+import express from "express";
+import userController from "../controllers/userController.js";
+import { verifyToken } from "../middleware/authMiddleware.js";
+import roleMiddleware from "../middleware/roleMiddleware.js";
+
+const router = express.Router();
+
+// Get all users
+router.get("/", verifyToken, userController.getAllUsers);
+
+// Get a single user by ID
+router.get("/:id", verifyToken, userController.getUserById);
+
+// Deactivate (soft delete) a user
+router.put(
+  "/deactivate/:id",
+  verifyToken,
+  roleMiddleware(["admin"]),
+  userController.deactivateUser
+);
+
+// Activate (soft delete) a user
+router.put(
+  "/activate/:id",
+  verifyToken,
+  roleMiddleware(["admin"]),
+  userController.reactivateUser
+);
+
+// Permanently delete a user
+router.delete(
+  "/:id",
+  verifyToken,
+  roleMiddleware(["admin"]),
+  userController.deleteUser
+);
+
+export default router;
